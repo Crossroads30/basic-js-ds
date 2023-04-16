37f2e253fb5f80f121b6e03e54aeabbebc70873f
Crossroads30 <Crossroads30@mail.ru>
2023-04-16 19:35:28 +0300

    feat

--- src/stack.js
+++ src/stack.js
@@ -1,34 +1,43 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Implement the Stack with a given interface via array.
- *
- * @example
- * const stack = new Stack();
- *
- * stack.push(1); // adds the element to the stack
- * stack.peek(); // returns the peek, but doesn't delete it, returns 1
- * stack.pop(); // returns the top element from stack and deletes it, returns 1
- * stack.pop(); // undefined
- *
- */
-class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+//  Implement the Stack with a given interface via array.
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+//  @example
+//   const stack = new Stack();
+
+//  stack.push(1); // adds the element to the stack
+//  stack.peek(); // returns the peek, but doesn't delete it, returns 1
+//  stack.pop(); // returns the top element from stack and deletes it, returns 1
+//  stack.pop(); // undefined
+function Stack() {
+   this.count = 0;
+   this.storage = {};
+
+   this.push = function (value) {
+      this.storage[this.count] = value;
+      this.count++;
+   }
+
+   this.pop = function () {
+      if (this.count === 0) {
+         return undefined;
+      }
+      this.count--;
+      var result = this.storage[this.count];
+      delete this.storage[this.count];
+      return result;
+   }
+
+   this.peek = function () {
+      return this.storage[this.count - 1];
+   }
+
+   this.size = function () {
+      return this.count;
+   }
 }
 
 module.exports = {
-  Stack
+   Stack
 };
